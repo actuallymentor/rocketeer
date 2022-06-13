be8f0839e435f3f6f68858b7cc0e87c99a669d75
Mentor <mentor@palokaj.co>
2022-06-13 09:15:21 +0200

    ✨ script update and memory increase for notifier

--- scripts/backup-images.zsh
+++ scripts/backup-images.zsh
@@ -3,12 +3,14 @@
 ## I intend to make this an online thing
 ## But the storage cost is a bit expensive
 ## ###############
+brew update
+brew install pigz
 timestamp=$( date )
 cd .backups && \
 gcloud config set project rocketeer-nft && \
 gsutil -m cp -r gs://rocketeer-nft.appspot.com/mainnetRocketeers/ ./rocketeer-image-backups/ && \
-tar -cv rocketeer-image-backups | gzip -9 > rocketeer-backup-$timestamp.tar.gz && \
-mv rocketeer-backup-$timestamp.tar.gz ~/Google\ Drive/Backup/Rocketeers/
-echo "✅ success" || echo "🛑 fail"
+tar -cv rocketeer-image-backups | pigz -9 > rocketeer-backup-$timestamp.tar.gz && \
+cp rocketeer-backup-$timestamp.tar.gz ~/Google\ Drive/My\ Drive/Backup/Rocketeers/ && \
+echo "✅ $(date) success" || echo "🛑 $(date) fail"
 
 say "Backup process done"
